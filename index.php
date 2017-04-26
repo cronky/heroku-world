d6b914902db2e97d2e3c15c494c19735da3568e7
cronky <oliver@cronky.net>
2017-04-26 21:44:23 +0100

    Update index.php
    Made some changes to see if this gets deployed straight away!

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 print "Heroku World :-) This is only a test.";
+print '<br/>And now I am deploying another version of the code based on the GitHub repo at: <a href="https://github.com/cronky/heroku-world/">https://github.com/cronky/heroku-world/</a>';
 ?>
