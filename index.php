7fe433717f63947e29f1d29e4bd26ac173faa6a7
Oliver Cronk <oliver@cronky.net>
2017-12-13 11:34:41 +0000

    Update index.php
    Removing Allan demo and putting in basic HTML tags

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<?php
-print "Heroku World :-) This is only a test. Hi Allan!";
+<html>
+  <head><title>Cronky Heroku Test</title>
+  </head>
+  <body><?php
+print "Heroku World :-) This is only a test!";
 print '<br/>And now I am deploying another version of the code based on the GitHub repo at: <a href="https://github.com/cronky/heroku-world/">https://github.com/cronky/heroku-world/</a>';
-?>
+    ?></body>
+</html>
