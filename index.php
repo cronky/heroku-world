f1865940aa171a3d12148be687af796ad7926ac2
Oliver Cronk <oliver@cronky.net>
2018-04-30 16:46:14 +0100

    Update index.php
    added date

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <body><?php
 print "Heroku World :-) This is only a test!";
 print '<br/>And now I am deploying another version of the code based on the GitHub repo at: <a href="https://github.com/cronky/heroku-world/">https://github.com/cronky/heroku-world/</a>';
-print "<br/>And another - this time to show Andy what I mean't!<br/>";
+print "<br/>Date:";
+print date();
     ?></body>
 </html>
