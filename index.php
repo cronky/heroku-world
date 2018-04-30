7cad768ca504f545f36bb462a826cbab0a60b102
Oliver Cronk <oliver@cronky.net>
2018-04-30 20:28:23 +0100

    Update index.php
    passed a parameter to the date function!

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 print "Heroku World :-) This is only a test!";
 print '<br/>And now I am deploying another version of the code based on the GitHub repo at: <a href="https://github.com/cronky/heroku-world/">https://github.com/cronky/heroku-world/</a>';
 print "<br/>Date:";
-print date();
+print date('r');
     ?></body>
 </html>
