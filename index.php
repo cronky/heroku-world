e15f5335e897efe6e6e4659a431316e377a927a2
Oliver Cronk <oliver@cronky.net>
2018-04-30 13:15:03 +0100

    Update index.php
    Added HTML tags

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
   <body><?php
 print "Heroku World :-) This is only a test!";
 print '<br/>And now I am deploying another version of the code based on the GitHub repo at: <a href="https://github.com/cronky/heroku-world/">https://github.com/cronky/heroku-world/</a>';
-print "And another - this time to show Andy what I mean't!";
+print "<br/>And another - this time to show Andy what I mean't!<br/>";
     ?></body>
 </html>
